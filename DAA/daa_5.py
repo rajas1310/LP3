7f0f38e0822c2ae7faa06ca0f220e8610c84b565
Rajas Chitale <56029517+rajas1310@users.noreply.github.com>
2022-11-23 20:17:54 +0530

    Update daa_5.py

--- DAA/daa_5.py
+++ DAA/daa_5.py
@@ -1,34 +1,32 @@
 import random
 
-
-def partition(array, low, high, piv):
-    pivot = array[high]
-    i = low - 1
-    for j in range(low, high):
-        if array[j] <= pivot:
+def partition(arr, low, high):
+    if randomized:
+        pivot = random.randrange(low, high)
+        arr[pivot], arr[high] = arr[high], arr[pivot]
+    pivot = arr[high]
+    i = low - 1 
+    for j in range(low,high):
+        if arr[j] <= pivot:
             i = i + 1
-            (array[i], array[j]) = (array[j], array[i])
-    (array[i + 1], array[high]) = (array[high], array[i + 1])
-    return i + 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i+1],arr[high] = arr[high], arr[i+1]
+    return i+1
 
 
-def quickSort(array, low, high):
-	if low < high:
-		if randomized:
-			piv = random.randrange(low, high)
-		else:
-			piv = high
-		pi = partition(array, low, high, piv)
-		quickSort(array, low, pi - 1)
-		quickSort(array, pi + 1, high)
+def quicksort(arr, low, high):
+    if low<high:
+        p = partition(arr, low, high)
+        quicksort(arr,low, p-1)
+        quicksort(arr, p+1, high)
 
+randomized = bool(input("Random = 1; Determistic = 0; ==> "))
 
-randomized = bool(int(input("Enter 0 for fixed, 1 for randomized pivot : ")))
-data = [1, 7, 4, 1, 10, 9, -2]
-print("Unsorted Array")
+data = [3 ,5, -1, 8, 40, -2, 6]
+print("Unsorted data")
 print(data)
-size = len(data)
-randomized = True
-quickSort(data, 0, size - 1)
-print("Sorted Array in Ascending Order:")
+
+quicksort(data, 0, len(data)-1)
+
+print("Sorted")
 print(data)
