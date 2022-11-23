b32d5908d5f2cadbe38b8069180a34e4956458f1
Rajas Chitale <56029517+rajas1310@users.noreply.github.com>
2022-11-23 20:11:19 +0530

    Update daa_1.py

--- DAA/daa_1.py
+++ DAA/daa_1.py
@@ -1,28 +1,20 @@
-def iterative_fibonacci(n):
-    a = 0
-    b = 1
-    c = 0
-    if n < 0:
-        return "Incorrect input"
-    if n == 1:
-        return b
-    if n == 0:
-        return a
+def fiboRecursive(n):
+    if n == 0 or n==1:
+        return n
+    else:
+        return fiboRecursive(n-1) + fiboRecursive(n-2)
     
-    for i in range(2, n+1):
-        c = a + b
-        a = b
-        b = c
-    return c
-    
-def recursive_fibonacci(n):
-    if n < 0:
-        return "Incorrect input"
-    elif n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    return recursive_fibonacci(n - 1) + recursive_fibonacci(n - 2)
-
-print(iterative_fibonacci(15))
-print(recursive_fibonacci(12))
+def fiboIterative(n):
+    if n == 0 or n==1:
+        return n
+    else:
+        n1 = 0
+        n2 = 1
+        for i in range(2, n+1):
+            n3 = n1 + n2
+            n1 = n2
+            n2 = n3
+    return n3
+        
+print(fiboIterative(6))
+print(fiboRecursive(7))
