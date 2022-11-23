7ab9bbf9bd18b3279e6339b60245691ed4a2d223
Rajas Chitale <56029517+rajas1310@users.noreply.github.com>
2022-11-23 20:10:37 +0530

    placed one queen randomly

--- DAA/daa_4.py
+++ DAA/daa_4.py
@@ -1,70 +1,43 @@
-global n
+import random
 
-def printSolution(board):
-    for i in range(n):
-        for j in range(n):
-            print(board[i][j], end=" ")
-        print()
+N = int(input("Enter the number of queens to be placed = "))
 
+board = [[0 for i in range(N)] for _ in range(N)]
 
-def isSafe(board, row, col):
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
-    i, j = row - 1, col + 1
-    while i >= 0 and j < n:
-        if board[i][j] == 1:
-            return False
-        i -= 1
-        j += 1
-    i, j = row + 1, col + 1
-    while i < n and j < n:
-        if board[i][j] == 1:
-            return False
-        i += 1
-        j += 1
-    i, j = row - 1, col - 1
-    while i >= 0 and j >= 0:
-        if board[i][j] == 1:
-            return False
-        i -= 1
-        j -= 1
-    i, j = row + 1, col - 1
-    while i < n and j >= 0:
-        if board[i][j] == 1:
-            return False
-        i += 1
-        j -= 1
-    return True
+def isAttacked(i,j):
+    for k in range(N):
+        if board[i][k] == 1 or board[k][j] == 1:
+            return True
 
-
-def solvenQUtil(board, col):
-    if col >= n:
-        return True
-    for i in range(n):
-        if isSafe(board, i, col):
-            board[i][col] = 1
-            if solvenQUtil(board, col + 1) == True:
+        for l in range(N):
+            if (k+l == i+j or k-l==i-j) and board[k][l] == 1:
                 return True
-            board[i][col] = 0
     return False
 
+def nQueens(n):
+    if n == 0:
+        return True
 
-def solvenQ():
-    board = []
-    global n
-    n = int(input("Enter the number of queens : "))
-    for _ in range(n):
-        temp = []
-        for _ in range(n):
-            temp.append(0)
-        board.append(temp)
-    if solvenQUtil(board, 0) == False:
-        print("Solution does not exist") 
-        return False
+    for i in range(N):
+        for j in range(N):
+            if (isAttacked(i,j)==False and board[i][j] != 1):
+                board[i][j] = 1
+                if nQueens(n-1) == True:
+                    return True
+                board[i][j] = 0
 
-    printSolution(board)
-    return True
+    return False
 
+rand_row = 2 #random.randrange(0,N)
+rand_col = 0 #random.randrange(0,N)
+print("First Q placed at: (",rand_row,",",rand_col,")")
+board[rand_row][rand_col] = 1
 
-solvenQ()
+status = nQueens(N-1) # N-1 because 1 Queen is already placed
+
+if status:
+    for i in board:
+        print(i)
+
+else:
+    print("Solution not possible...")
